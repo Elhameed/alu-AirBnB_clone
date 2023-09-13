2d05c98b1171fa55e5d085cea58fc01c298ee980
Elhameed <a.ajani@alustudent.com>
2023-09-13 21:59:52 +0100

    new commit

--- models/base_model.py
+++ models/base_model.py
@@ -5,21 +5,30 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        """Initialize a new instance of BaseModel"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(
+                            self, key,
+                            datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                        )
+                    else:
+                        setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        """Return a string representation of the object"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update the 'updated_at' attribute with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Return a dictionary representation of the object"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
