27551e37a059d6597828d191144248d772253c2f
Elhameed <a.ajani@alustudent.com>
2023-09-13 20:06:08 +0100

    back to base

--- models/base_model.py
+++ models/base_model.py
@@ -1,36 +1,27 @@
 #!/usr/bin/python3
+
 import uuid
 from datetime import datetime
-from models import storage
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+        """Initialize a new instance of BaseModel"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
+        """Return a string representation of the object"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """Update the 'updated_at' attribute with the current datetime"""
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
+        """Return a dictionary representation of the object"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-
-    @classmethod
-    def from_dict(cls, data_dict):
-        return cls(**data_dict)
